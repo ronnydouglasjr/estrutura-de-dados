44e758c604160ef1ca0a3e2dd816222ccda8b452
Ronny <ronnydougla2@gmail.com>
2026-05-12 17:21:04 -0300

    Melhoria na estrutura de dados bubble sort

--- src/main/java/org/example/ordenacao/bubbleSort/BubbleSort.java
+++ src/main/java/org/example/ordenacao/bubbleSort/BubbleSort.java
@@ -6,11 +6,14 @@ public class BubbleSort {
 
     for (int i = 0; i < array.length; i++) {
       boolean trocou = false;
-      for (int j = 0; j < array.length - 1 - i; j++) {
-        if (array[j] > array[j + 1]) {
-          int temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
+      for (int j = 1; j < array.length; j++) {
+
+        int indice = j - 1;
+
+        if (array[indice] > array[j]) {
+          int temp = array[indice];
+          array[indice] = array[j];
+          array[j] = temp;
           trocou = true;
         }
       }
